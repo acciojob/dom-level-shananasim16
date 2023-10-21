462515f1bf268d7e414887b4cedc94a4aceccf82
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-21 12:15:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+// script.js
+window.addEventListener('DOMContentLoaded', () => {
+    const targetElement = document.getElementById('level');
+
+    if (targetElement) {
+        let domLevel = 0;
+        let currentElement = targetElement;
+
+        while (currentElement !== document.documentElement) {
+            currentElement = currentElement.parentNode;
+            domLevel++;
+        }
+
+        // Display the result using the alert function
+        alert(`The level of the element is: ${domLevel}`);
+    } else {
+        alert('Element with ID "level" not found.');
+    }
+});
+
